89f6686ffe17dc2a7a744c01c4ad0b8095916f83
Mehmet Ege Acican <mehmet.ege.acican@gmail.com>
2023-07-12 20:14:02 +0300

    route init

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import { Box, CssBaseline, ThemeProvider, Toolbar, createTheme } from "@mui/material";
 
 import React from "react";
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Statistics from "./pages/Statistics";
 import Navbar from "./layout/Navbar";
 import SideNav from "./layout/SideNav";
+
 
 
 const defaultTheme = createTheme();
@@ -17,24 +18,28 @@ function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Box sx={{ display: 'flex', maxWidth: "100vw" }}>
-        <CssBaseline />
-        <Navbar open={open} toggleDrawer={toggleDrawer} />
-        <SideNav open={open} toggleDrawer={toggleDrawer} />
-        <Box
-          alignContent={"center"}
-          sx={{
-            backgroundColor: (theme) =>
-              theme.palette.mode === 'light'
-                ? theme.palette.grey[200]
-                : theme.palette.grey[800],
-            width: "100vw"
-          }}
-        >
-          <Toolbar />
-          <Statistics />
+      <BrowserRouter>
+        <Box sx={{ display: 'flex', maxWidth: "100vw" }}>
+          <CssBaseline />
+          <Navbar open={open} toggleDrawer={toggleDrawer} />
+          <SideNav open={open} toggleDrawer={toggleDrawer} />
+          <Box
+            alignContent={"center"}
+            sx={{
+              backgroundColor: (theme) =>
+                theme.palette.mode === 'light'
+                  ? theme.palette.grey[200]
+                  : theme.palette.grey[800],
+              width: "100vw"
+            }}
+          >
+            <Toolbar />
+            <Routes>
+              <Route path="/" element={<Statistics />} />
+            </Routes>
+          </Box>
         </Box>
-      </Box>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }

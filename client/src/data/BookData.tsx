37051b7c0655d203617d515d3e697f44b40d584a
Mehmet Ege Acican <mehmet.ege.acican@gmail.com>
2023-07-27 15:14:26 +0300

    create form ui completed

--- client/src/data/BookData.tsx
+++ client/src/data/BookData.tsx
@@ -1,7 +1,7 @@
 
 import BackupTableIcon from '@mui/icons-material/BackupTable';
 import BookIcon from '@mui/icons-material/Book';
-import { Box, Button, Checkbox, Chip, Container, Divider, FormControlLabel, FormGroup, Paper, Stack, Typography } from '@mui/material';
+import { Box, Button, Chip, Container, Divider, Stack, Typography } from '@mui/material';
 import React, { useCallback, useEffect } from 'react';
 import { TabInterface } from '../interfaces/TabInterfaces';
 import LibraryAccordion from '../components/accordions/LibraryAccordion';
@@ -12,17 +12,30 @@ import StringValueField from '../components/forms/StringValueField';
 import MultipleSelectionAutocomplete from '../components/forms/MultipleSelectionAutocomplete';
 import { ICategory } from '../interfaces/DataInterfaces';
 
+
+/**
+ * This Section includes the React Components Specific to Books Section,
+ * which the Books.tsx page uses
+ */
+
+/**
+ * Book Tabs for the Book Page
+ */
 export const BookTabs: TabInterface[] = [
     { icon: (<BackupTableIcon />), label: "Table View" },
     { icon: (<BookIcon />), label: "Card View" },
 ];
-
+/**
+ * Tab Contents for the Book Page
+ */
 export const BookTabContents: JSX.Element[] = [
     (<Typography component={'span'} variant={'body2'}> <BookAccordions /> </Typography>),
     (<Typography component={'span'} variant={'body2'}> CARD </Typography>)
 ];
 
-
+/**
+ * Table Headers for Books
+ */
 export const BookTableHeader: string[] = [
     "Book ID",
     "Name",
@@ -33,24 +46,15 @@ export const BookTableHeader: string[] = [
     "Notes"
 ];
 
-export const BookDataExamples: any = [
-    { id: 1, name: "Book 1", author: "Author 1", category: "View", Status: "Red", entered: "21.07.2023", notes: "view" },
-    { id: 2, name: "Book 2", author: "Author 1", category: "View", Status: "Reading", entered: "21.07.2023", notes: "view" },
-    { id: 3, name: "Book 3", author: "Author 1", category: "View", Status: "Will Read", entered: "21.07.2023", notes: "view" },
-    { id: 4, name: "Book 4", author: "Author 1", category: "View", Status: "Reading", entered: "21.07.2023", notes: "view" },
-    { id: 5, name: "Book 5", author: "Author 1", category: "View", Status: "Will Read", entered: "21.07.2023", notes: "view" },
-    { id: 6, name: "Book 6", author: "Author 1", category: "View", Status: "Will Read", entered: "21.07.2023", notes: "view" },
-    { id: 7, name: "Book 7", author: "Author 2", category: "View", Status: "Will Read", entered: "21.07.2023", notes: "view" },
-    { id: 8, name: "Book 8", author: "Author 2", category: "View", Status: "Will Read", entered: "21.07.2023", notes: "view" },
-    { id: 9, name: "Book 7", author: "Author 2", category: "View", Status: "Red", entered: "21.07.2023", notes: "view" },
-    { id: 10, name: "Book 8", author: "Author 2", category: "View", Status: "Red", entered: "21.07.2023", notes: "view" },
 
-]
-
+/**
+ * Accordion Datas for Book Page
+ */
 export const BookAccordionDatas: AccordionData[] = [
-    { title: "View Books", info: "View the Books in Table Format", data: (<DataTable headers={BookTableHeader} tableDatas={BookDataExamples} />) },
+    { title: "View Books", info: "View the Books in Table Format", data: (<DataTable headers={BookTableHeader} tableDatas={[]} />) },
     { title: "Add Book", info: "Add a new Book", data: (<CreateBookForm />) }
 ]
+
 
 export const defaultBookCategories = [
     { id: 1, name: "Psychology" },
@@ -58,12 +62,18 @@ export const defaultBookCategories = [
     { id: 3, name: "Anthropology" },
 ];
 
+/**
+ * Create Form for Create Book
+ * @returns rendered create book form component
+ */
 export function CreateBookForm() {
     // Variables -- Hooks 
     const [bookName, setBookName] = React.useState<string>('White Fang');
     const [author, setAuthor] = React.useState<string>('Jack London');
-    const [selectedCategories,setSelectedCategories] = React.useState<ICategory[]>([]);
-    const [selectedStatus,setSelectedStatus] = React.useState<string>("Reading");
+    const [selectedCategories, setSelectedCategories] = React.useState<ICategory[]>([]);
+    const [selectedStatus, setSelectedStatus] = React.useState<string>("Reading");
+
+
     return (
         <Box
             component="form"
@@ -75,21 +85,21 @@ export function CreateBookForm() {
                         <StringValueField label='Please Enter the Author name' data={author} setter={setAuthor} />
                     </Stack>
 
-                    <MultipleSelectionAutocomplete 
-                        label="Select Categories" 
-                        placeholder='categories' 
-                        categories={defaultBookCategories} 
-                        selected={selectedCategories} 
-                        setSelected={setSelectedCategories} 
+                    <MultipleSelectionAutocomplete
+                        label="Select Categories"
+                        placeholder='categories'
+                        categories={defaultBookCategories}
+                        selected={selectedCategories}
+                        setSelected={setSelectedCategories}
                     />
-                    <Divider/>
+                    <Divider />
                     <Stack direction={'row'} spacing={2} alignContent={'center'}>
-                        <Chip clickable onClick = {() => setSelectedStatus("Red")} label="Red" color="error" variant={selectedStatus === "Red" ? "filled" : "outlined" } />
-                        <Chip clickable onClick = {() => setSelectedStatus("Reading")} label="Reading" color="warning" variant={selectedStatus === "Reading" ? "filled" : "outlined" }  />
-                        <Chip clickable onClick = {() => setSelectedStatus("Will Reading")} label="Will Read" color="success" variant={selectedStatus === "Will Reading" ? "filled" : "outlined" }  />
+                        <Chip clickable onClick={() => setSelectedStatus("Red")} label="Red" color="error" variant={selectedStatus === "Red" ? "filled" : "outlined"} />
+                        <Chip clickable onClick={() => setSelectedStatus("Reading")} label="Reading" color="warning" variant={selectedStatus === "Reading" ? "filled" : "outlined"} />
+                        <Chip clickable onClick={() => setSelectedStatus("Will Reading")} label="Will Read" color="success" variant={selectedStatus === "Will Reading" ? "filled" : "outlined"} />
                     </Stack>
-                    <Divider/>
-                    <Button sx={{alignItems:"center",width:300}} variant='outlined'> Add </Button> 
+                    <Divider />
+                    <Button sx={{ alignItems: "center", maxWidth: 300 }} variant='outlined' onClick={() => console.log("Clicked")}> Add </Button>
                 </Stack>
             </Container>
         </Box>
@@ -97,6 +107,10 @@ export function CreateBookForm() {
     )
 };
 
+/**
+ * Accordion for Book Page
+ * @returns Rendered Accordion designed for Books 
+ */
 export default function BookAccordions() {
     //Hooks
     const [expanded, setExpanded] = React.useState<string | false>(false);

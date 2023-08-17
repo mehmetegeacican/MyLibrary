14a089fe5bc4d9d062ad4f58aa360bcb57d6e12a
Mehmet Ege Acican <mehmet.ege.acican@gmail.com>
2023-08-17 19:25:07 +0300

    post new category added

--- services/categoryservice/src/controller/categoryController.ts
+++ services/categoryservice/src/controller/categoryController.ts
@@ -1,5 +1,7 @@
 import express from 'express';
-import { getAll } from '../models/categoryModel';
+import { addNewCategory, getAll } from '../models/categoryModel';
+import { validationResult } from 'express-validator';
+
 /**
  * This is the controller for getting all of the categories
  * @param req the Request
@@ -10,8 +12,32 @@ export const getAllCategories = async (req:express.Request,res:express.Response)
         const data = await getAll();
         res.json(data);
     }
-    catch(error){
+    catch{
         const err = "Db Connection not established";
         res.status(500).json({ error: err });
+    }
+}
+
+/**
+ * This function posts a new category
+ * @param req Request
+ * @param res Response
+ */
+export const postNewCategory = async (req:express.Request,res:express.Response) => {
+    try{
+        const {name,info} = req.body;
+        console.log(name,info);
+        //Step 1 -- Validate
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({ errors: errors.array() });
+        }
+        //Step 2 -- If Checks are applied, create a new category
+        const result = await addNewCategory(name,info);
+        return res.status(201).json({data:result});
+    }
+    catch(e){
+        const err = "Db Connection not established";
+        res.status(500).json({error:err});
     }
 }

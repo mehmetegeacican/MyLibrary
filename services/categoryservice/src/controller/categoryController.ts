43dc5f07eaeeef978aeded3e4446fb4ac0672e2f
Mehmet Ege Acican <mehmet.ege.acican@gmail.com>
2023-08-17 13:19:04 +0300

    modal added, controller completed

--- services/categoryservice/src/controller/categoryController.ts
+++ services/categoryservice/src/controller/categoryController.ts
@@ -1,14 +1,17 @@
 import express from 'express';
+import { getAll } from '../models/categoryModel';
 /**
  * This is the controller for getting all of the categories
  * @param req the Request
  * @param res the Response
  */
-export const getAllCategories = (req:express.Request,res:express.Response) => {
+export const getAllCategories = async (req:express.Request,res:express.Response) => {
     try{
-        res.json({message:"All Categories Retrieved"});
+        const data = await getAll();
+        res.json(data);
     }
     catch(error){
-        res.send({error:error});
+        const err = "Db Connection not established";
+        res.status(500).json({ error: err });
     }
 }

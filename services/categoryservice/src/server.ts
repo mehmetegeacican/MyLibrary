6cbaa59dea5a5f27867a4f6fe8c47f54d01be0c2
mehmetegeacican <47591567+mehmetegeacican@users.noreply.github.com>
2023-08-17 12:20:51 +0300

    93 prisma init (#96)
    * env added
    
    * prisma initialized
    
    * prisma schema added
    
    ---------
    
    Co-authored-by: Mehmet Ege Acican <mehmet.ege.acican@gmail.com>

--- services/categoryservice/src/server.ts
+++ services/categoryservice/src/server.ts
@@ -1,8 +1,13 @@
 import app from './app';
+import dotenv from 'dotenv';
 
+//Env Variables config
+dotenv.config();
+const port = process.env.PORT;
+//Start the server
 try {
-    app.listen(4000, () => {
-      console.log(`Server is running on port ${4000}`);
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
     });
   } catch (error) {
     console.error('Error occurred:', error);

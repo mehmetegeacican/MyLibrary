39afe2fd6928e06cfe17a37ce9cbddaaa6d2dd4e
Mehmet Ege Acican <mehmet.ege.acican@gmail.com>
2023-08-18 15:01:12 +0300

    docker completed

--- services/categoryservice/src/server.ts
+++ services/categoryservice/src/server.ts
@@ -6,8 +6,8 @@ dotenv.config();
 const port = process.env.PORT;
 //Start the server
 try {
-    app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
+    app.listen(4000, () => {
+      console.log(`Server is running on port ${4000}`);
     });
   } catch (error) {
     console.error('Error occurred:', error);

6d07f150a0368f81c7a29c989e2b2288413a22bd
Mehmet Ege Acican <mehmet.ege.acican@gmail.com>
2023-08-18 11:25:21 +0300

    prisma function added

--- services/categoryservice/src/models/categoryModel.ts
+++ services/categoryservice/src/models/categoryModel.ts
@@ -114,3 +114,32 @@ export const deleteCategory = async (id: number) => {
         await prisma.$disconnect();
     }
 };
+
+/**
+ * Prisma Function to Update category
+ * @param id 
+ * @param requestBody 
+ */
+export const updateCategory =async (id:number,requestBody:{name:string,info:string}) => {
+    try{
+        const {name,info} = requestBody;
+        const res = await prisma.category.update({
+            where : {
+                id:id
+            } ,
+            data : {
+                name: name,
+                info: info
+            }
+        })
+        return res;
+    }
+    catch(e){
+        console.log(e);
+        throw new Error("Db Connection Unsuccessful");
+    }
+    finally{
+        await prisma.$disconnect();
+    }
+    
+}

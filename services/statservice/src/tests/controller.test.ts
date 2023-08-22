c3da9d09595881f7ab49a0af8d42cdbb10b9f5be
Mehmet Ege Acican <mehmet.ege.acican@gmail.com>
2023-08-22 14:52:41 +0300

    tests added

--- services/statservice/src/tests/controller.test.ts
+++ services/statservice/src/tests/controller.test.ts
@@ -1,7 +1,8 @@
 import request from 'supertest';
 import app from '../app';
 import {
-    executeGetAuthorCounts as executeGetAuthorCountsMock
+    executeGetAuthorCounts as executeGetAuthorCountsMock,
+    executeGetCategoryCounts as executeGetCategoryCountsMock
 } from '../model/statModel';
 
 
@@ -12,19 +13,27 @@ const mockStatByAuthor = [
     { name:"Author 3", count: 1},
 ];
 
+const mockStatByCategory = [
+    { name:"Category 1", count: 3},
+    { name:"Category 2", count: 2},
+    { name:"Category 3", count: 1},
+]
+
 // Step 2 -- Mock Functions 
 jest.mock('../model/statModel', () => ({
     executeGetAuthorCounts: jest.fn(),
+    executeGetCategoryCounts : jest.fn()
 }));
 
 /**
  * Mock Versions of Functions
  */
 const executeGetAuthorCounts = executeGetAuthorCountsMock as jest.Mock; // Cast to jest.Mock
+const executeGetCategoryCounts = executeGetCategoryCountsMock as jest.Mock;
 
 
 
-describe('GET --> /api/v1/stats/all', () => {
+describe('GET --> /api/v1/stats/all/authors', () => {
     beforeEach(async () => {
         jest.clearAllMocks();
         jest.spyOn(console, 'log').mockImplementation(() => { }); // Suppress log messages
@@ -36,7 +45,7 @@ describe('GET --> /api/v1/stats/all', () => {
         //Given
         executeGetAuthorCounts.mockResolvedValue(mockStatByAuthor);
         //When
-        const response = await request(app).get('/api/V1/stats/all');
+        const response = await request(app).get('/api/V1/stats/all/authors');
         //Then
         expect(response.status).toBe(200);
         expect(response.body).toEqual(mockStatByAuthor);
@@ -48,11 +57,44 @@ describe('GET --> /api/v1/stats/all', () => {
         const err = "Db Connection not established";
         executeGetAuthorCounts.mockRejectedValue(err);
         //When
-        const response = await request(app).get('/api/V1/stats/all');
+        const response = await request(app).get('/api/V1/stats/all/authors');
         //Then
         expect(response.status).toBe(500);
         expect(response.body).toEqual({error:err});
         //Verify
         expect(executeGetAuthorCounts).toHaveBeenCalledTimes(1);
+    })
+});
+
+describe('GET --> /api/v1/stats/all/categories', () => {
+    beforeEach(async () => {
+        jest.clearAllMocks();
+        jest.spyOn(console, 'log').mockImplementation(() => { }); // Suppress log messages
+    });
+    afterEach(() => {
+        jest.restoreAllMocks(); // Restore console.log after each test
+    });
+    it('Should successfully retrieve all the datas', async () => {
+        //Given
+        executeGetCategoryCounts.mockResolvedValue(mockStatByCategory);
+        //When
+        const response = await request(app).get('/api/V1/stats/all/categories');
+        //Then
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual(mockStatByCategory);
+        //Verify
+        expect(executeGetCategoryCounts).toHaveBeenCalledTimes(1);
+    });
+    it('Should give a db error in case of connection failure',async () => {
+        //Given
+        const err = "Db Connection not established";
+        executeGetCategoryCounts.mockRejectedValue(err);
+        //When
+        const response = await request(app).get('/api/V1/stats/all/categories');
+        //Then
+        expect(response.status).toBe(500);
+        expect(response.body).toEqual({error:err});
+        //Verify
+        expect(executeGetCategoryCounts).toHaveBeenCalledTimes(1);
     })
 });
--- services/statservice/src/tests/modal.test.ts
+++ services/statservice/src/tests/modal.test.ts
@@ -1,5 +1,5 @@
 import  {connectDb as connectDbMock, closeDb as closeDbMock} from '../dbconnection';
-import { executeGetAuthorCounts } from '../model/statModel';
+import { executeGetAuthorCounts, executeGetCategoryCounts } from '../model/statModel';
 
 //Mock the ConnectDb & CloseDb
 jest.mock('../dbconnection', () => ({
@@ -16,6 +16,12 @@ const mockStatByAuthor = [
     { name:"Author 2", count: 2},
     { name:"Author 3", count: 1},
 ];
+
+const mockStatByCategory = [
+    { name:"Category 1", count: 3},
+    { name:"Category 2", count: 2},
+    { name:"Category 3", count: 1},
+]
 
 describe('executeGetAuthorCounts', () => {
     beforeEach(async () => {
@@ -53,6 +59,51 @@ describe('executeGetAuthorCounts', () => {
 
         // When & Then
         await expect(executeGetAuthorCounts()).rejects.toThrow('Db Connection not established');
+
+        // Verify
+        expect(connectDb).toHaveBeenCalledTimes(1);
+        expect(mockClient.query).toHaveBeenCalledTimes(1);
+        expect(closeDb).toHaveBeenCalledTimes(1);
+        expect(closeDb).toHaveBeenCalledWith(mockClient);
+    });
+});
+
+describe('executeGetCategoryCounts', () => {
+    beforeEach(async () => {
+        connectDb.mockClear();
+        closeDb.mockClear();
+        jest.spyOn(console, 'log').mockImplementation(() => { }); // Suppress log messages
+    });
+    afterEach(() => {
+        jest.restoreAllMocks(); // Restore console.log after each test
+    });
+    it('should retrieve the stats from the db',async () => {
+        //Given
+        const mockQueryResult = { rows: mockStatByCategory };
+        const mockClient = { query: jest.fn().mockResolvedValue(mockQueryResult) };
+        connectDb.mockResolvedValue(mockClient);
+       
+        //When
+        const result = await executeGetCategoryCounts();
+        //Then
+        //expect(mockClient.query).toHaveBeenCalledWith('select author ,count(b.author) as total  from books b group by b."author" order by total desc;');
+        expect(result).toEqual(mockStatByCategory);
+        expect(closeDb).toHaveBeenCalledWith(mockClient);
+        //Verify
+        expect(connectDb).toHaveBeenCalledTimes(1);
+        expect(mockClient.query).toHaveBeenCalledTimes(1);
+        expect(closeDb).toHaveBeenCalledTimes(1);
+
+    });
+    it('Should handle db error', async () => {
+        // Given
+        const mockClient = { query: jest.fn() };
+        connectDb.mockResolvedValue(mockClient);
+        closeDb.mockResolvedValue; // Assume closeDb resolves successfully
+        mockClient.query.mockRejectedValue('DB Error');
+
+        // When & Then
+        await expect(executeGetCategoryCounts()).rejects.toThrow('Db Connection not established');
 
         // Verify
         expect(connectDb).toHaveBeenCalledTimes(1);

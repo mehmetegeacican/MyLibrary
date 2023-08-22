0cffbdc68dd58cac5e5f6a1b7a699b089f1f8937
mehmetegeacican <47591567+mehmetegeacican@users.noreply.github.com>
2023-08-22 15:14:05 +0300

    129 dougnut chart for categories (#130)
    * pie chart added
    
    * dashboard menu
    
    ---------
    
    Co-authored-by: Mehmet Ege Acican <mehmet.ege.acican@gmail.com>

--- client/src/apis/statApi.ts
+++ client/src/apis/statApi.ts
@@ -3,12 +3,29 @@ import axios from "axios";
 const PORT = import.meta.env.VITE_STATSERVICE_PORT;
 const ADDRESS = `http://localhost:${PORT}`;
 
+/**
+ * the API FOR BookCount by Author
+ * @returns The Book Count by author
+ */
 export const fetchAllBookCountsByAuthor = async () => {
     try {
-        const res = await axios.get(ADDRESS + '/api/v1/stats/all');
+        const res = await axios.get(ADDRESS + '/api/v1/stats/all/authors');
         return res.data;
     }
     catch {
+        return [];
+    }
+}
+/**
+ * The API for Book Count by Category
+ * @returns 
+ */
+export const fetchAllBookCountsByCategory = async () => {
+    try{
+        const res = await axios.get(ADDRESS + '/api/v1/stats/all/categories');
+        return res.data;
+    }
+    catch{
         return [];
     }
 }

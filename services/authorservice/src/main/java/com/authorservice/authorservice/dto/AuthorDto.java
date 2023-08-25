dbc00445155b2c05146e016c54fe2cdeaecfc603
Mehmet Ege Acican <mehmet.ege.acican@gmail.com>
2023-08-25 18:01:43 +0300

    DTO added

---
+++ services/authorservice/src/main/java/com/authorservice/authorservice/dto/AuthorDto.java
@@ -0,0 +1,16 @@
+package com.authorservice.authorservice.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class AuthorDto {
+    private Long id;
+    private String name;
+}

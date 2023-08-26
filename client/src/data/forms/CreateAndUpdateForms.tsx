3044c2ffd869983d8e0ff333e80819912bc43366
Mehmet Ege Acican <mehmet.ege.acican@gmail.com>
2023-08-26 18:20:26 +0300

    author form init

--- client/src/data/forms/CreateAndUpdateForms.tsx
+++ client/src/data/forms/CreateAndUpdateForms.tsx
@@ -85,6 +85,45 @@ export function BookForm({format, data, handleClose}:FormInterface) {
 };
 
 
+export function AuthorForm({format, data, handleClose}:FormInterface){
+    //Hooks & Contexts
+    const [formName,setFormName] = React.useState<string>("");
+    const [formInfo,setFormInfo] = React.useState<string>("");
+    const [formMessage, setFormMessage] = React.useState<string>("");
+    const [formError, setFormError] = React.useState<boolean>(false);
+    const [formSuccess,setFormSuccess] = React.useState<boolean>(false);
+    const { error,message, success} = useCreateAndUpdateForm(formError, setFormError, formMessage, setFormMessage,formSuccess,setFormSuccess);
+
+    //submit
+    const submit = () => {
+
+    }
+
+    return(
+        <Box
+        >
+            <Container>
+                <Stack spacing={2} alignContent={'center'}>
+                    <Stack direction={'row'} spacing={2} alignItems={'center'}>
+                        <StringValueField label='Please Enter the Author name' data={formName} setter={setFormName} />
+                        <StringValueField label='Please Enter the Author Info' data={formInfo} setter={setFormInfo} />
+                    </Stack>
+                    <Divider />
+                    <Stack direction={'row'} spacing={2} alignContent={'center'}>
+                        
+                    </Stack>
+                    <Divider />
+                    {format === "create" && (<Button sx={{ alignItems: "center", maxWidth: 300 }} variant='outlined' onClick={submit}> Add </Button>)}
+                    {format === "update" && (<Button sx={{ alignItems: "center", maxWidth: 300 }} variant='outlined' onClick={submit}> Update </Button>)}
+                    {error && <Alert sx={{ mt: 2 }} severity="error"> {message}</Alert>}
+                    {success && <Alert sx={{ mt: 2 }} severity="success"> {message}</Alert>}
+                </Stack>
+            </Container>
+        </Box>
+    )
+}
+
+
 export function CategoryForm({format,data,handleClose}:FormInterface){
 
     //Hooks and Contexts

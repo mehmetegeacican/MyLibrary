6f79b2bf904ce78be1fce36b57c07ac3544c3ed2
Mehmet Ege Acican <mehmet.ege.acican@gmail.com>
2023-11-05 16:11:31 +0300

    backend bug fix

--- services/authorservice/src/main/java/com/authorservice/authorservice/repository/AuthorRepository.java
+++ services/authorservice/src/main/java/com/authorservice/authorservice/repository/AuthorRepository.java
@@ -2,6 +2,9 @@ package com.authorservice.authorservice.repository;
 
 import com.authorservice.authorservice.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
@@ -10,4 +13,6 @@ public interface AuthorRepository extends JpaRepository<Author,Long> {
     boolean existsByNameIgnoreCaseAndUserId(String name, Long userId);
 
     List<Author> findAllByUserId(Long userId);
+
+    List<Author> findByNameAndUserId(String name,Long userId);
 }

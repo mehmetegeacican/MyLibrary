6a1b6c01eecac46110c3747e8bc16ad32025e0ff
Mehmet Ege Acican <mehmet.ege.acican@gmail.com>
2024-02-11 12:59:44 +0300

    category service backend updated for 401

--- services/categoryservice/src/routes/routes.ts
+++ services/categoryservice/src/routes/routes.ts
@@ -9,8 +9,10 @@ import {
     CategoryDataIDValidate,
     CategoryDataRequestBodyValidate
  } from '../validators/category.validation';
-
+import reqAuth from '../middleware/reqAuth';
 const router:express.Router = express.Router();
+// Middleware
+router.use(reqAuth);
 
 router.get('/all/:id',getAllCategories);
 router.post('/',CategoryDataRequestBodyValidate(),postNewCategory);

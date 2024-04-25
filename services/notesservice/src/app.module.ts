bcf637a6fa1cabe6aad2385a997a8b6a6bb04ac6
Mehmet Ege Acican <mehmet.ege.acican@gmail.com>
2024-04-25 18:18:36 +0300

    db config init

--- services/notesservice/src/app.module.ts
+++ services/notesservice/src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject:[ConfigService],
+      useFactory: (configService: ConfigService) => ({
+          type:'postgres',
+          host:configService.get('PGHOST'),
+          port: + configService.get('PGPORT'),
+          username: configService.get('PGUSER'),
+          password: configService.get('PGPASSWORD'),
+          database: configService.get('PGDATABASE'),
+          entities: [],
+          // synchronize is used for migration
+      })
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

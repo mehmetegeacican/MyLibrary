afd785a852b4123bb38b88b823f53a57bd17e3c9
Mehmet Ege Acican <mehmet.ege.acican@gmail.com>
2024-05-12 08:44:08 +0300

    apis added

--- client/src/apis/noteApis.ts
+++ client/src/apis/noteApis.ts
@@ -26,6 +26,45 @@ export const fetchAllNotes = async (id:number,token:string) => {
 }
 
 /**
+ * API function to post a new note
+ * @param reqBody The Request Body for the new Note
+ * @param token 
+ * @returns 
+ */
+export const postNewNote = async (reqBody:object,token:string) => {
+    try {
+        const config = {
+            headers: { 'Authorization': 'Bearer ' + token }
+        };
+        const res = await axios.post(NOTE_ADDRESS + '/api/v2/notes', reqBody,config);
+        return res.data;
+    }
+    catch (e) {
+        return e;
+    }
+}
+
+/**
+ * API Function to update a new note
+ * @param id The Note ID
+ * @param reqBody Request Body
+ * @param token Auth Token
+ * @returns 
+ */
+export const updateNote = async (id:string,reqBody:object,token:string) => {
+    try {
+        const config = {
+            headers: { 'Authorization': 'Bearer ' + token }
+        };
+        const res = await axios.put(NOTE_ADDRESS + `/api/v2/notes/${id}`, reqBody,config);
+        return res.data;
+    }
+    catch (e) {
+        return e;
+    }
+}
+
+/**
  * The Function Required to Delete A Note
  * @param id the Note Id
  * @param token The JWT Token

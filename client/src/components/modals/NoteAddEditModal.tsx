7b32a8bfd2f8714dbf99a3cfe72d9fda278466bf
Mehmet Ege Acican <mehmet.ege.acican@gmail.com>
2024-05-12 09:12:57 +0300

    add note page init

--- client/src/components/modals/NoteAddEditModal.tsx
+++ client/src/components/modals/NoteAddEditModal.tsx
@@ -1,4 +1,4 @@
-import { Dialog, DialogTitle } from '@mui/material'
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Divider, TextField } from '@mui/material'
 
 
 
@@ -8,8 +8,43 @@ interface NoteModalInterface {
 }
 export default function NoteAddEditModal({open,handleClose}:NoteModalInterface) {
   return (
-    <Dialog open={open} onClose={handleClose}>
+    <Dialog open={open} onClose={handleClose} maxWidth={'md'} fullWidth >
         <DialogTitle>Add Note</DialogTitle>
+        <Divider/>
+        <DialogContent>
+          <DialogContentText>
+              Add or Update Notes from here. Enter Note Content in Markdown format
+          </DialogContentText>
+          <TextField
+              autoFocus
+              required
+              margin="dense"
+              id="title"
+              name="Title"
+              label="Note Title"
+              type="text"
+              fullWidth
+              variant="standard"
+            />
+        </DialogContent>
+        <DialogContent>
+          <TextField
+              autoFocus
+              required
+              multiline
+              rows={12}
+              id="content"
+              name="Content"
+              label="Note Content"
+              type="text"
+              fullWidth
+              variant="outlined"
+            />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button>Add</Button>
+        </DialogActions>
     </Dialog>
   )
 }

2a33fe25bbae1ece3be5b94af37409280cb26de4
Mehmet Ege Acican <mehmet.ege.acican@gmail.com>
2024-09-08 14:13:55 +0300

    color init

--- client/src/pages/ProfilePage.tsx
+++ client/src/pages/ProfilePage.tsx
@@ -1,8 +1,17 @@
 import { Container, FormControlLabel, Grid, Paper, Radio, RadioGroup, Typography } from '@mui/material'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { SignUpForm } from './SignUpPage'
+import { useAuthContext } from '../hooks/contextHooks/useAuthContext'
 
 export default function ProfilePage() {
+
+    const {themeColor,dispatch} = useAuthContext();
+
+
+    useEffect(() => {
+        console.log("Theme Color is: ", themeColor);
+    },[themeColor]);
+
     return (
         <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
             <Grid container spacing={3}>
@@ -39,8 +48,13 @@ export default function ProfilePage() {
                     >
                         <Typography sx={{
                             marginBottom:2
-                        }} variant='h6' color={'secondary'}> Select Theme Color </Typography>
-                        <RadioGroup name="use-radio-group" defaultValue="secondary">
+                        }} variant='h6' color={"secondary"}> Select Theme Color </Typography>
+                        <RadioGroup name="use-radio-group" defaultValue="secondary" onChange={(e) => {
+                            dispatch({
+                                type:'SET_THEME_COLOR',
+                                payload:e.target.value
+                            })
+                        }}>
                             <div style={{
                                 display: 'flex',
                                 flexDirection: 'row'
@@ -48,7 +62,7 @@ export default function ProfilePage() {
                                 <FormControlLabel value="primary" control={<Radio  color='primary' size='medium'/>} label="Blue" />
                                 <FormControlLabel value="secondary" control={<Radio  color='secondary' size='medium'/>} label="Purple" />
                                 <FormControlLabel value="success" control={<Radio  color='success' size='medium'/>} label="Green" />
-                                <FormControlLabel value="danger" control={<Radio  color='error' size='medium'/>} label="Red" />
+                                <FormControlLabel value="error" control={<Radio  color='error' size='medium'/>} label="Red" />
                                 <FormControlLabel value="warning" control={<Radio  color='warning' size='medium'/>} label="Orange" />
                             </div>
 

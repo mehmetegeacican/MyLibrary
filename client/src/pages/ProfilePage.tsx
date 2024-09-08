f7bfd69675d3c5057a34caf926fd79b5c4a5a8f8
Mehmet Ege Acican <mehmet.ege.acican@gmail.com>
2024-09-08 13:51:25 +0300

    theme color init

--- client/src/pages/ProfilePage.tsx
+++ client/src/pages/ProfilePage.tsx
@@ -1,10 +1,10 @@
-import { Container, Grid, Paper } from '@mui/material'
+import { Container, FormControlLabel, Grid, Paper, Radio, RadioGroup, Typography } from '@mui/material'
 import React from 'react'
 import { SignUpForm } from './SignUpPage'
 
 export default function ProfilePage() {
-  return (
-    <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+    return (
+        <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
             <Grid container spacing={3}>
                 <Grid item xs={12} md={12} lg={12}>
                     <Paper
@@ -37,9 +37,25 @@ export default function ProfilePage() {
                             height: 120,
                         }}
                     >
+                        <Typography sx={{
+                            marginBottom:2
+                        }} variant='h6' color={'secondary'}> Select Theme Color </Typography>
+                        <RadioGroup name="use-radio-group" defaultValue="secondary">
+                            <div style={{
+                                display: 'flex',
+                                flexDirection: 'row'
+                            }}>
+                                <FormControlLabel value="primary" control={<Radio  color='primary' size='medium'/>} label="Blue" />
+                                <FormControlLabel value="secondary" control={<Radio  color='secondary' size='medium'/>} label="Purple" />
+                                <FormControlLabel value="success" control={<Radio  color='success' size='medium'/>} label="Green" />
+                                <FormControlLabel value="danger" control={<Radio  color='error' size='medium'/>} label="Red" />
+                                <FormControlLabel value="warning" control={<Radio  color='warning' size='medium'/>} label="Orange" />
+                            </div>
+
+                        </RadioGroup>
                     </Paper>
                 </Grid>
             </Grid>
         </Container>
-  )
+    )
 }

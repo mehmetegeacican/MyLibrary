269d4e83a9c245fe558cc5da98c0bed7d6f88c36
Mehmet Ege Acican <mehmet.ege.acican@gmail.com>
2024-10-12 13:10:47 +0300

    entity init

--- services/categoryservicenest/src/categories/entities/category.entity.ts
+++ services/categoryservicenest/src/categories/entities/category.entity.ts
@@ -1,1 +1,18 @@
-export class Category {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({name: 'Category'})
+export class Category {
+    @PrimaryGeneratedColumn()
+    id:number;
+
+    @Column()
+    name:String;
+
+    @Column()
+    user_id:number;
+
+    @Column()
+    info:string;
+
+}
+

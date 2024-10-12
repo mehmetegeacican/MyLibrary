f99cc89bb26f0d41fff4786fb6476874acc2b45c
Mehmet Ege Acican <mehmet.ege.acican@gmail.com>
2024-10-12 20:33:14 +0300

    middleware added

--- services/categoryservicenest/src/app.module.ts
+++ services/categoryservicenest/src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { CategoriesModule } from './categories/categories.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Category } from './categories/entities/category.entity';
+import { AuthMiddleware } from 'common/middleware/auth.middleware';
 
 @Module({
   imports: [
@@ -29,4 +30,8 @@ import { Category } from './categories/entities/category.entity';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes('/api/v2/categories')
+  }
+}

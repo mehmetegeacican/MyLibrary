3252884bffc8c1804c63e8de1870e94719478c42
Mehmet Ege Acican <mehmet.ege.acican@gmail.com>
2024-11-16 17:29:32 +0300

    entity added

--- services/userservice/src/users/entities/user.entity.ts
+++ services/userservice/src/users/entities/user.entity.ts
@@ -1,1 +1,22 @@
-export class User {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity({name:'User'})
+export class User {
+    @PrimaryGeneratedColumn()
+    id:number;
+
+    @Column()
+    username:string;
+
+    @Column()
+    password:string;
+
+    @Column()
+    theme_color:string;
+
+    @CreateDateColumn({ name: 'created_at' })
+    createdAt:Date;
+
+    @UpdateDateColumn({name:'updated_at'})
+    updatedAt:Date;
+}

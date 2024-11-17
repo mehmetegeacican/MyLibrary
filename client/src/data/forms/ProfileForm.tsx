060ab813138b5f698dcd4d8f0921905dd29cbd2a
Mehmet Ege Acican <mehmet.ege.acican@gmail.com>
2024-11-17 09:21:44 +0300

    debounce and username change added

--- client/src/data/forms/ProfileForm.tsx
+++ client/src/data/forms/ProfileForm.tsx
@@ -1,4 +1,4 @@
-import { Avatar, FormControl, IconButton, Input, InputAdornment, InputLabel, Stack , Button} from '@mui/material'
+import { Avatar, FormControl, IconButton, Input, InputAdornment, InputLabel, Stack, Button } from '@mui/material'
 import React, { useMemo, useState } from 'react'
 import { useAuthContext } from '../../hooks/contextHooks/useAuthContext'
 import PersonIcon from '@mui/icons-material/Person';
@@ -7,18 +7,43 @@ import { VisibilityOff, Visibility } from '@mui/icons-material';
 import { useLibraryTheme } from '../../hooks/theme/useLibraryTheme';
 import UploadButton from '../../components/buttons/uploadButton';
 import { updateUser } from '../../apis/userApis';
+import { useDebounce } from '../../hooks/asyncHooks/useDebounce';
+import { message } from 'antd';
 
 
 
 
 export default function ProfileForm() {
   const { user } = useAuthContext();
-  const {libTheme} = useLibraryTheme();
+  const { libTheme } = useLibraryTheme();
   // Variables
-  const [username,setUsername] = useState(user?.email || "");
-  const [password,setPassword] = useState("");
+  const [username, setUsername] = useState(user?.email || "");
+  const [debouncedUsername, setDebouncedUsername] = useState(user?.email || '');
+
+  const [password, setPassword] = useState("");
 
   const [showPassword, setShowPassword] = React.useState(false);
+
+  // Handlers
+
+  // Use debounce to delay the update of debouncedUsername
+  const debounceUsername = useDebounce(async (value: string) => {
+    setDebouncedUsername(value);
+    if (user?.id) {
+      try {
+        // Update the username via API
+        await updateUser(user?.id.toString(), { username: value }, user?.token);
+
+        // Update username in localStorage
+        const updatedUser = { ...user, email: value };
+        localStorage.setItem('user', JSON.stringify(updatedUser));
+        message.success('Username changed successfully!');
+      } catch (error) {
+        console.error("Failed to update user:", error);
+        message.error('Error occured! Could not change the username');
+      }
+    }
+  }, 4000);
 
   const handleClickShowPassword = () => setShowPassword((show) => !show);
 
@@ -30,28 +55,29 @@ export default function ProfileForm() {
     event.preventDefault();
   };
 
-  const avatarColor = useMemo(() => {
-      switch(libTheme){
-        case 'primary':
-          return '#2196f3';
-        case 'secondary':
-          return '#9c27b0';
-        case 'success':
-          return '#4caf50';
-        case 'error':
-          return '#c62828';
-        case 'warning':
-          return '#ff6f00';
-        default:
-          return 'blueviolet'
-      }
-  },[libTheme]);
-
-  const handleUserInfoSave = async () => {
-     let obj = {
-       username: username
-     }
+  const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setUsername(value);
+    debounceUsername(value);
   };
+
+  const avatarColor = useMemo(() => {
+    switch (libTheme) {
+      case 'primary':
+        return '#2196f3';
+      case 'secondary':
+        return '#9c27b0';
+      case 'success':
+        return '#4caf50';
+      case 'error':
+        return '#c62828';
+      case 'warning':
+        return '#ff6f00';
+      default:
+        return 'blueviolet'
+    }
+  }, [libTheme]);
+
 
   return (
     <Stack direction={'row'} spacing={7} alignItems={'center'} justifyContent={'space-between'}>
@@ -70,7 +96,7 @@ export default function ProfileForm() {
             id="standard-adornment-username"
             color={libTheme}
             value={username}
-            onChange={(e) => setUsername(e.target.value)}
+            onChange={handleUsernameChange}
           />
         </FormControl>
         <FormControl variant="standard">
@@ -95,18 +121,7 @@ export default function ProfileForm() {
             }
           />
         </FormControl>
-        <UploadButton title='Upload Profile Picture'/>
-        <Button 
-          variant='contained'
-          color={libTheme}
-          style={{
-            width:'30vw',
-            alignSelf:'center'
-          }}
-          onClick={() => handleUserInfoSave()}
-        >
-          Update
-        </Button>
+        <UploadButton title='Upload Profile Picture' />
       </Stack>
 
     </Stack>

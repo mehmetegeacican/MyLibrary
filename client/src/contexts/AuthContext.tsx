d3d2bdffd46a34e613de82b3cf95296b60f19864
Mehmet Ege Acican <mehmet.ege.acican@gmail.com>
2024-11-17 17:46:42 +0300

    initial color theme add

--- client/src/contexts/AuthContext.tsx
+++ client/src/contexts/AuthContext.tsx
@@ -14,56 +14,35 @@ export const AuthDataContext = createContext<AuthContextType | undefined>(undefi
  * @returns 
  */
 const authReducer = (state: AuthState, action: AuthAction) => {
-    switch (action.type) {
-      case 'LOGIN':
-        return {...state, user: action.payload};
-      case 'LOG_OUT':
-        return {...state, user: null};
-      case 'SIGN_UP':
-        return {...state, user: action.payload};
-      case 'SET_THEME_COLOR':
-        return {...state, themeColor: action.payload};
-      default:
-        return state;
-    }
+  switch (action.type) {
+    case 'LOGIN':
+      return { ...state, user: action.payload };
+    case 'LOG_OUT':
+      return { ...state, user: null };
+    case 'SIGN_UP':
+      return { ...state, user: action.payload };
+    case 'SET_THEME_COLOR':
+      return { ...state, themeColor: action.payload };
+    default:
+      return state;
+  }
 };
 
 
-export const AuthContextProvider: React.FC<AuthContextProviderProps> = ({children}) => {
-    const [state, dispatch] = useReducer(authReducer, {
-        user: null,
-        themeColor:'secondary'
-    });
+export const AuthContextProvider: React.FC<AuthContextProviderProps> = ({ children }) => {
+  const [state, dispatch] = useReducer(authReducer, {
+    user: null,
+    themeColor: 'secondary'
+  });
 
-    const fetchUserData = async (id:string,token:string) => {
-        try{
-          const result = await getUserById(id,token);
-          if(result.theme_color){
-            dispatch({type:'SET_THEME_COLOR',payload:result.theme_color});
-          }
-        } catch(e){
-          return e;
-        }
-    }
 
-    useEffect(() => {
-      //Parse the Local Storages json String
-      let userData;
-      const user = localStorage.getItem('user');
-      if(user){
-        userData = JSON.parse(user);
-      }
-      //If this is present, then we have a user present, initial dispatch 
-      if (user) {
-          fetchUserData(userData.id,userData.token);
-          dispatch({ type: 'LOGIN', payload: userData });
-      }
 
-  }, []);
-    return (
-        <AuthDataContext.Provider value={{ ...state, dispatch }}>
-            {children}
-        </AuthDataContext.Provider>
 
-    );
+
+  return (
+    <AuthDataContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </AuthDataContext.Provider>
+
+  );
 }
--- client/src/hooks/theme/useLibraryTheme.tsx
+++ client/src/hooks/theme/useLibraryTheme.tsx
@@ -1,13 +1,33 @@
-import React, { useMemo } from 'react'
+import React, { useEffect, useMemo } from 'react'
 import { useAuthContext } from '../contextHooks/useAuthContext';
 import { OverridableStringUnion } from '@mui/types';
 import { AppBarPropsColorOverrides } from '@mui/material';
+import { getUserById } from '../../apis/userApis';
 
 export const useLibraryTheme = () => {
-    const { themeColor  } = useAuthContext();
+    const { user, dispatch, themeColor } = useAuthContext();
+
+
+    const fetchUserData = async (id: string, token: string) => {
+        try {
+            const result = await getUserById(id, token);
+            if (result.theme_color) {
+                dispatch({ type: 'SET_THEME_COLOR', payload: result.theme_color });
+            }
+        } catch (e) {
+            return e;
+        }
+    }
+
+
+    useEffect(() => {
+        if(user?.id && user){
+            fetchUserData(user?.id.toString(),user?.token);
+        }
+    },[user]);
 
     const libTheme: OverridableStringUnion<"primary" | "secondary" | "error" | "warning" | "success" | "transparent", AppBarPropsColorOverrides> = useMemo(() => {
-        switch(themeColor) {
+        switch (themeColor) {
             case 'primary':
                 return 'primary';
             case 'secondary':

fc2c79af0787e05d07492c4773839aed7bf7f8ce
Mehmet Ege Acican <mehmet.ege.acican@gmail.com>
2024-12-16 15:38:22 +0300

    backend updates

--- services/authservice/model/authModel.js
+++ services/authservice/model/authModel.js
@@ -1,4 +1,5 @@
 const { connectDb, closeDb } = require('../dbconnection');
+const bcrypt = require('bcrypt');
 /**
  * Model to Add A New User to The DB
  */
@@ -24,28 +25,32 @@ const addNewUser = async (username, password) => {
 /**
  * Model to Check If a User with the Username Password combination Exists
  */
-const checkIfUserExists = async (username, password) => {
+/**
+ * Model to Check If a User with the Username Exists and Compare Passwords
+ */
+const checkIfUserExists = async (username, plainPassword) => {
     let client = await connectDb();
     try {
-        const checkQuery = `SELECT ID FROM "User"
-        WHERE username = $1 AND password = $2`;
-        const values = [username,password];
+        // Query to fetch the hashed password
+        const checkQuery = `SELECT ID, password FROM "User" WHERE username = $1`;
+        const values = [username];
         const result = await client.query(checkQuery, values);
-        if(result.rowCount > 0){
-            return true
+
+        if (result.rowCount > 0) {
+            const user = result.rows[0]; // Fetch the user data
+            const isMatch = await bcrypt.compare(plainPassword,user.password); // Compare passwords
+            return isMatch ? true : false; // Return user ID if match, otherwise false
+        } else {
+            return false; // User does not exist
         }
-        else{
-            return false;
-        }
-    }
-    catch (e) {
+    } catch (e) {
         console.log(e);
         throw new Error("Db Connection Unsuccessful");
-    }
-    finally {
+    } finally {
         await closeDb(client);
     }
-}
+};
+
 
 const getIdOfUser = async (username) => {
     let client = await connectDb();

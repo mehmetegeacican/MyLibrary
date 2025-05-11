8f206c4331724a1ccf1b922f79dd9be3ed7ecc12
Mehmet Ege Acican <mehmet.ege.acican@gmail.com>
2025-05-11 09:42:41 +0300

    isConnected added

--- services/bookservice/redisconnection.js
+++ services/bookservice/redisconnection.js
@@ -9,8 +9,10 @@ const connectRedis = async () => {
     try {
         await redisClient.connect();
         console.log('Connected to Redis');
+        return true;
     } catch (err) {
         console.error('Redis connection error:', err);
+        return false;
     }
 };
 
@@ -23,51 +25,64 @@ const closeRedis = async () => {
     }
 };
 
-// Primary Redis Functions for Caching
 const getCache = async (key, id = null) => {
+    let isConnected = false;
     try {
-        await connectRedis();  
-        const cacheKey = id ? `${key}:${id}` : key;
-        console.log("Cache key is :" , cacheKey);
-        const cacheValue = await redisClient.get(cacheKey);
-        if (cacheValue) {
-            return JSON.parse(cacheValue);
-        } 
-    } catch(err){
-        console.error("Error getting cache:", err)
-    }
-    finally {
-        await closeRedis();
+        isConnected = await connectRedis();
+        console.log(isConnected, "Is Connected : ", isConnected);
+        if (isConnected) {
+            const cacheKey = id ? `${key}:${id}` : key;
+            console.log("Cache key is :", cacheKey);
+            const cacheValue = await redisClient.get(cacheKey);
+            if (cacheValue) {
+                return JSON.parse(cacheValue);
+            }
+        }
+    } catch (err) {
+        console.error("Error getting cache:", err);
+    } finally {
+        if (isConnected) {
+            await closeRedis();
+        }
         return;
     }
-}
+};
 
-//  Primary Redis Function for Setting Cache
 const setCache = async (key, value, id = null) => {
+    let isConnected = false;
     try {
-        await connectRedis();  
-        const cacheKey = id ? `${key}:${id}` : key;
-        await redisClient.set(cacheKey, JSON.stringify(value), {EX: 3600});
-        console.log(`Cache set for key: ${cacheKey}`);
-    } catch(err){
-        console.error("Error setting cache:", err)
+        isConnected = await connectRedis();
+        if (isConnected) {
+            const cacheKey = id ? `${key}:${id}` : key;
+            await redisClient.set(cacheKey, JSON.stringify(value), { EX: 3600 });
+            console.log(`Cache set for key: ${cacheKey}`);
+        }
+    } catch (err) {
+        console.error("Error setting cache:", err);
     } finally {
-        await closeRedis();
+        if (isConnected) {
+            await closeRedis();
+        }
     }
-}
+};
 
 // Primary Redis Function for clearing Cache
-const clearCache = async (key,id = null) => {
-    try{
-        await connectRedis();  
-        const cacheKey = id ? `${key}:${id}` : key;
-        await redisClient.del(cacheKey);
-        console.log(`Cache cleared for key: ${cacheKey}`);
-    } catch(err){
+const clearCache = async (key, id = null) => {
+    let isConnected = false;
+    try {
+        isConnected = await connectRedis();
+        if (isConnected) {
+            const cacheKey = id ? `${key}:${id}` : key;
+            await redisClient.del(cacheKey);
+            console.log(`Cache cleared for key: ${cacheKey}`);
+        }
+    } catch (err) {
         console.error("Error clearing cache:", err)
     }
     finally {
-        await closeRedis();
+        if (isConnected) {
+            await closeRedis();
+        }
     }
 };
 

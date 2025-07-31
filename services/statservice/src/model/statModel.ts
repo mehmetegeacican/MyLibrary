952c117789a1caf2624fc7d2139fba1475ea6c23
Mehmet Ege Acican <mehmet.ege.acican@gmail.com>
2025-07-31 20:18:47 +0300

    2 new apis added

--- services/statservice/src/model/statModel.ts
+++ services/statservice/src/model/statModel.ts
@@ -1,13 +1,13 @@
-import {connectDb,closeDb} from "../dbconnection";
+import { connectDb, closeDb } from "../dbconnection";
 
 /**
  * gets the Author Counts
  * @returns All total number of books based on the author
  */
-export const executeGetAuthorCounts = async (userId:number) => {
+export const executeGetAuthorCounts = async (userId: number) => {
     let client = await connectDb();
     let data;
-    try{
+    try {
         //Step 2 -- Get the Result
         const result = await client!.query(`SELECT c.author_name, COUNT(*) AS author_count
         FROM books b
@@ -17,10 +17,10 @@ export const executeGetAuthorCounts = async (userId:number) => {
         ORDER BY author_count DESC;`);
         data = result.rows;
         return data;
-    } catch(e){
+    } catch (e) {
         console.log(e);
         throw new Error("Db Connection not established");
-    } finally{
+    } finally {
         await closeDb(client);
     }
 }
@@ -29,10 +29,10 @@ export const executeGetAuthorCounts = async (userId:number) => {
  * gets the number of categroy counts
  * @returns the number of category count
  */
-export const executeGetCategoryCounts = async (userId:number) => {
+export const executeGetCategoryCounts = async (userId: number) => {
     let client = await connectDb();
     let data;
-    try{
+    try {
         const result = await client!.query(`SELECT c.category_name, COUNT(*) AS category_count
         FROM books b
         CROSS JOIN LATERAL unnest(b.category) AS c(category_name)
@@ -42,7 +42,7 @@ export const executeGetCategoryCounts = async (userId:number) => {
         data = result.rows;
         return data;
     }
-    catch(e){
+    catch (e) {
         console.log(e);
         throw new Error("Db Connection not established");
     } finally {
@@ -54,17 +54,75 @@ export const executeGetCategoryCounts = async (userId:number) => {
  * The SQL Query Function to return the stats of the statuses
  * @returns The Status counts
  */
-export const executeGetStatusCounts = async (userId:number) => {
+export const executeGetStatusCounts = async (userId: number) => {
     let client = await connectDb();
     let data;
-    try{
+    try {
         const result = await client!.query(`select b.status ,count(b.status) as total  from books b where user_id = ${userId} group by b."status" order by total  desc;`);
         data = result.rows;
         return data;
-    }catch(e){
+    } catch (e) {
         console.log(e);
         throw new Error("Db Connection not established");
-    }finally{
+    } finally {
+        await closeDb(client);
+    }
+};
+
+/**
+ * The SQL Query Function to return the average,sum and bookcaount  by authors
+ * @returns The Status counts
+ */
+export const executeGetAverageLikedOfAuthors = async (userId: number) => {
+    let client = await connectDb();
+    let data;
+    try {
+        const result = await client!.query(`
+            SELECT c.author_name, COUNT(*) AS book_count,
+            ROUND(AVG(b.liked), 3) AS avg_liked,
+            SUM(b.liked) AS total_liked
+            FROM books b
+            CROSS JOIN LATERAL unnest(b.authors) AS c(author_name)
+            WHERE b.user_id = $1
+            GROUP BY c.author_name
+            ORDER BY avg_liked DESC;
+            `, [userId]);
+
+        data = result.rows;
+        return data;
+    } catch (e) {
+        console.log(e);
+        throw new Error("Db Connection not established");
+    } finally {
+        await closeDb(client);
+    }
+};
+
+/**
+ * The SQL Query Function to return the average,sum and bookcaount  by categories
+ * @returns The Status counts
+ */
+export const executeGetAverageLikedOfCategories = async (userId: number) => {
+    let client = await connectDb();
+    let data;
+    try {
+        const result = await client!.query(`
+        SELECT c.category_name, COUNT(*) AS book_count,
+                ROUND(AVG(b.liked), 3) AS avg_liked,
+                SUM(b.liked) AS total_liked
+        FROM books b
+        CROSS JOIN LATERAL unnest(b.category) AS c(category_name)
+        WHERE b.user_id = $1
+        GROUP BY c.category_name
+        ORDER BY avg_liked DESC;
+        `, [userId]);
+
+        data = result.rows;
+        return data;
+    } catch (e) {
+        console.log(e);
+        throw new Error("Db Connection not established");
+    } finally {
         await closeDb(client);
     }
 };
--- services/statservice/src/routes/routes.ts
+++ services/statservice/src/routes/routes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getTotalBasedByAuthor, getTotalBasedByCategory, getTotalBasedByStatuses } from '../controller/statController';
+import { getTotalBasedByAuthor, getTotalBasedByCategory, getTotalBasedByStatuses, getAvgLikedOfAuthors, getAvgLikedOfCategories} from '../controller/statController';
 import  reqAuth from '../middleware/reqAuth';
 
 
@@ -12,5 +12,7 @@ router.use(reqAuth);
 router.get('/all/authors/:id',getTotalBasedByAuthor);
 router.get('/all/categories/:id',getTotalBasedByCategory);
 router.get('/all/statuses/:id',getTotalBasedByStatuses);
+router.get('/all/liked/authors/:id',getAvgLikedOfAuthors);
+router.get('/all/liked/categories/:id',getAvgLikedOfCategories);
 
 export default router;

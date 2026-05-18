03331749db7325937fad64294af01da91113b111
Mehmet Ege Acican <mehmet.ege.acican@gmail.com>
2026-05-18 11:27:05 +0300

    Soft Delete and Optimistic locking added

--- services/notesservice/src/annotations/entities/annotation.entity.ts
+++ services/notesservice/src/annotations/entities/annotation.entity.ts
@@ -1,25 +1,39 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn, 
+  VersionColumn     
+} from 'typeorm';
 
 @Entity('annotations')
 export class Annotation {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ type: 'integer', nullable: true }) 
-  userId: string;
+  @Column({ type: 'integer', nullable: true })
+  userId: number; 
 
   @Column({ type: 'varchar', nullable: true })
   bookId: string;
 
-  @Column({ type: 'text', nullable: true }) 
+  @Column({ type: 'text', nullable: true })
   comment: string;
 
-  @Column({ type: 'integer', nullable: true }) 
+  @Column({ type: 'integer', nullable: true })
   pageNumber: number;
 
   @CreateDateColumn()
   createdAt: Date;
 
   @UpdateDateColumn()
-  updatedat:Date;
+  updatedat: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+
+  @VersionColumn()
+  version: number;
 }
